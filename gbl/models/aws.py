22c3b851a6417ad17285f3c0a502c8e82ae61f7c
U-lis <ds2ilz@hotmail.com>
2016-07-02 01:08:50 +0900

    refresh db schema. delete redundant table/columns

--- gbl/models/aws.py
+++ gbl/models/aws.py
@@ -2,26 +2,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import types, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
-
-class BaseClass(object):
-    __table_args__ = {}
-
-    id = Column(types.Integer, primary_key=True, nullable=False, autoincrement=True)
-
-    def __repr__(self):
-        if self.id:
-            return "<class {} ID: {}>".format(type(self).__name__, self.id)
-        else:
-            return "<class {} ID: None>".format(type(self).__name__)
-
-
-Base = declarative_base(cls=BaseClass)
+Base = declarative_base()
 
 
 class Region(Base):
     __tablename__ = 'region'
 
-    name = Column(types.Unicode(16), nullable=False)
+    name = Column(types.Unicode(16), primary_key=True, nullable=False, index=True)
     endpoint = Column(types.Unicode(32), nullable=False)
 
     def __init__(self, region_name):
@@ -31,42 +18,37 @@ class Region(Base):
 class AvailabilityZone(Base):
     __tablename__ = 'availability_zone'
 
-    region_id = Column(types.Integer, ForeignKey('region.id'), nullable=False)
-    region = relationship('Region', foreign_keys=[region_id])
-    name = Column(types.Unicode(32), nullable=False)
+    region_name = Column(types.Unicode(16), ForeignKey('region.name'), nullable=False)
+    region = relationship('Region', foreign_keys=[region_name])
+    name = Column(types.Unicode(32), primary_key=True, nullable=False, index=True)
 
-    def __init__(self, region_id, zone_name):
-        self.region_id = region_id
+    def __init__(self, region_name, zone_name):
+        self.region_name = region_name
         self.name = zone_name
-
-
-class Product(Base):
-    __tablename__ = 'product'
-
-    description = Column(types.Unicode(16), nullable=False, index=True)
 
 
 class Instance(Base):
     __tablename__ = 'instance'
 
-    type = Column(types.Unicode(16), nullable=False)
+    type = Column(types.Unicode(16), primary_key=True, nullable=False)
     # TODO: add spec
 
 
 class SpotPrice(Base):
     __tablename__ = 'spot_price'
 
-    az_id = Column(types.Integer, ForeignKey('availability_zone.id'), nullable=False, index=True)
-    az = relationship('AvailabilityZone', foreign_keys=[az_id])
-    product_id = Column(types.Integer, ForeignKey('product.id'), nullable=False, index=True)
-    product = relationship('Product', foreign_keys=[product_id])
-    instance_id = Column(types.Integer, ForeignKey('instance.id'), nullable=False, index=True)
-    instance = relationship('Instance', foreign_keys=[instance_id])
+    id = Column(types.Integer, primary_key=True, autoincrement=True, nullable=False)
+    az_name = Column(types.Unicode(32), ForeignKey('availability_zone.name'), nullable=False, index=True)
+    az = relationship('AvailabilityZone', foreign_keys=[az_name])
+    product = Column(types.Unicode(16), nullable=False, index=True)
+    instance_type = Column(types.Unicode(16), ForeignKey('instance.type'), nullable=False, index=True)
+    instance = relationship('Instance', foreign_keys=[instance_type])
     timestamp = Column(types.DateTime, nullable=False)
     price = Column(types.Float, nullable=False)
 
-
+"""
 class SearchLog(Base):
     __tablename__ = 'search_log'
 
     # TODO: scrap search log
+"""
