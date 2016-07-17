ae8b4bf6e1571fc613c9c0dafdbd77719a234552
U-lis <ds2ilz@hotmail.com>
2016-07-18 00:13:00 +0900

    move config to gbl.common

--- gbl/common.py
+++ gbl/common.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from raven import Client
 
-from config import DB_ECHO, DB, SENTRY_DSN
+from .config import DB_ECHO, DB, SENTRY_DSN
 
 engine_options = {'echo': DB_ECHO, 'poolclass': NullPool}
 engine = create_engine(DB, **engine_options)
